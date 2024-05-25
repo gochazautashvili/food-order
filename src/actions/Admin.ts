47ab509d5c216915302406f86910e50ef4fea88a
Gocha <gochazautashvili2017@gmail.com>
2024-05-25 17:25:22 +0400

    admin

--- src/actions/Admin.ts
+++ src/actions/Admin.ts
@@ -4,11 +4,7 @@ import { currentUser } from "@clerk/nextjs/server";
 export const getAdmin = async () => {
   const user = await currentUser();
   const adminUserEmail = "gochazautashvili2017@gmail.com";
-  const admin = user?.emailAddresses[0].emailAddress !== adminUserEmail;
+  const admin = user?.emailAddresses[0].emailAddress === adminUserEmail;
 
-  if (admin) {
-    return true;
-  } else {
-    return false;
-  }
+  return admin;
 };
